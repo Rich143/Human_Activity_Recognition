8d0ed52da7dd0b383de22e57274d801a930f063b
Richard Matthews <richard_matthews@apple.com>
2025-04-11 12:38:10 -0400

    Comment out AI process for now since it crashes

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -186,7 +186,7 @@ int main(void)
     HAL_Delay(100);
     /* USER CODE END WHILE */
 
-  MX_X_CUBE_AI_Process();
+  /*MX_X_CUBE_AI_Process();*/
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
