60113d7a5910fde03e3323e9dad62cce88d360fc
Richard Matthews <richard_matthews@apple.com>
2025-04-12 17:36:56 -0400

    Initial implementation of imu manager, untested

---
+++ Core/Inc/imu_manager.h
@@ -0,0 +1,41 @@
+/*
+ * imu_manager.h
+ *
+ *  Created on: Apr 12, 2025
+ *      Author: richardmatthews
+ */
+
+#ifndef INC_IMU_MANAGER_H_
+#define INC_IMU_MANAGER_H_
+
+#include "b-u585i-iot02a-bsp/b_u585i_iot02a_motion_sensors.h"
+
+/**
+ * IMU Sampling parameters. These must match the Model parameters
+ */
+//! Number of IMU samples per window
+#define IMU_WINDOW_SIZE 24
+//! IMU sampling frequency, this will correspond to 26 Hz for the ISM330DHCX
+#define IMU_SAMPLING_FREQUENCY_HZ 20.0f
+
+typedef BSP_MOTION_SENSOR_Axes_t IMU_Window[IMU_WINDOW_SIZE];
+
+/**
+ * @brief Initializes the IMU manager
+ *
+ * Initializes the IMU manager by initializing the IMU sensors
+ *
+ * @return BSP status.
+ */
+int imu_manager_init(void);
+
+/**
+ * @brief Reads in the IMU window from the sensors
+ *
+ * @param window Pointer to IMU window to write data into
+ *
+ * @return BSP status
+ */
+int imu_manager_get_window(IMU_Window* window);
+
+#endif /* INC_IMU_MANAGER_H_ */
---
+++ Core/Src/imu_manager.c
@@ -0,0 +1,44 @@
+/*
+ * imu_manager.c
+ *
+ *  Created on: Apr 12, 2025
+ *      Author: richardmatthews
+ */
+
+#include "imu_manager.h"
+#include "stm32u5xx_hal.h"
+
+#define IMU_SAMPLING_PERIOD_MS (1000 / IMU_SAMPLING_FREQUENCY_HZ)
+
+int imu_manager_init(void) {
+  int32_t status = BSP_ERROR_NONE;
+
+  status = BSP_MOTION_SENSOR_Init(0, MOTION_ACCELERO);
+  if (status != BSP_ERROR_NONE) return status;
+
+  status = BSP_MOTION_SENSOR_Enable(0, MOTION_ACCELERO);
+  if (status != BSP_ERROR_NONE) return status;
+
+  status = BSP_MOTION_SENSOR_SetOutputDataRate(0, MOTION_ACCELERO, IMU_SAMPLING_FREQUENCY_HZ);
+  if (status != BSP_ERROR_NONE) return status;
+
+  return status;
+}
+
+int imu_manager_read_window(IMU_Window *window) {
+  int32_t status = BSP_ERROR_NONE;
+  uint32_t sample_time = 0;
+
+  for (int i = 0; i < IMU_WINDOW_SIZE; i++) {
+    BSP_MOTION_SENSOR_Axes_t *axes = (BSP_MOTION_SENSOR_Axes_t *)&window[i];
+
+    sample_time = HAL_GetTick();
+
+    status = BSP_MOTION_SENSOR_GetAxes(0, MOTION_ACCELERO, axes);
+    if (status != BSP_ERROR_NONE) return status;
+
+    HAL_Delay(IMU_SAMPLING_PERIOD_MS - (HAL_GetTick() - sample_time));
+  }
+
+  return status;
+}
