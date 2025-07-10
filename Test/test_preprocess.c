12d559e6ac4d6cfe5764678f1d24fd5515767069
Richard Matthews <richard_matthews@apple.com>
2025-07-10 14:45:29 -0400

    Refactoring gravity down test

--- Test/test_preprocess.c
+++ Test/test_preprocess.c
@@ -37,16 +37,32 @@ TEST_SOURCE_FILE("../Drivers/CMSIS-DSP/Source/FilteringFunctions/arm_biquad_casc
     } while (0)
 
 typedef struct {
-    accel_data_t *expected_signal_in;
-    accel_data_t *expected_signal_out;
+    accel_data_t *input_signal;
+    accel_data_t *expected_output_signal;
     accel_data_t *output_signal;
 } test_signal_t;
+
+typedef struct {
+    accel_data_t *batch_in;
+    accel_data_t *batch_out;
+} batch_signal_t;
+
+typedef struct {
+    accel_data_t *valid_output;
+    accel_data_t *valid_expected_output;
+} valid_output_t;
 
 preprocess_t preprocess;
 
 accel_data_t scratch_buffer;
 
 test_signal_t test_signals;
+
+batch_signal_t batch_signals;
+
+valid_output_t valid_outputs;
+
+accel_data_t preprocess_output;
 
 accel_data_t *get_scratch_buffer(uint32_t num_samples) {
     scratch_buffer.x = (float32_t *)malloc(sizeof(float32_t) * num_samples);
@@ -59,21 +75,201 @@ accel_data_t *get_scratch_buffer(uint32_t num_samples) {
 }
 
 
+test_signal_t *get_test_signals(float32_t *x_in, float32_t *y_in, float32_t *z_in,
+                                float32_t *x_expected, float32_t *y_expected, float32_t *z_expected,
+                                uint32_t num_samples)
+{
+    const uint32_t N = num_samples;
+
+    static accel_data_t signal;
+
+    signal.num_samples = N;
+    signal.x = x_in;
+    signal.y = y_in;
+    signal.z = z_in;
+
+    static accel_data_t expected_output;
+
+    expected_output.num_samples = N;
+    expected_output.x = x_expected;
+    expected_output.y = y_expected;
+    expected_output.z = z_expected;
+
+    preprocess_output.x = (float32_t *)malloc(sizeof(float32_t) * N);
+    preprocess_output.y = (float32_t *)malloc(sizeof(float32_t) * N);
+    preprocess_output.z = (float32_t *)malloc(sizeof(float32_t) * N);
+
+    for (uint32_t i = 0; i < N; ++i) {
+        preprocess_output.x[i] = FLT_MAX;
+        preprocess_output.y[i] = FLT_MAX;
+        preprocess_output.z[i] = FLT_MAX;
+    }
+
+    static accel_data_t out;
+
+    out.num_samples = N;
+    out.x = preprocess_output.x;
+    out.y = preprocess_output.y;
+    out.z = preprocess_output.z;
+
+    test_signals.input_signal = &signal;
+    test_signals.expected_output_signal = &expected_output;
+    test_signals.output_signal = &out;
+
+    return &test_signals;
+}
+
+batch_signal_t *get_batch_signals(accel_data_t *in, accel_data_t *out, uint32_t start, uint32_t batch_size) {
+    static accel_data_t input_batch;
+
+    input_batch.num_samples = batch_size;
+    input_batch.x = &in->x[start];
+    input_batch.y = &in->y[start];
+    input_batch.z = &in->z[start];
+
+    static accel_data_t output_batch;
+
+    output_batch.num_samples = batch_size;
+    output_batch.x = &out->x[start];
+    output_batch.y = &out->y[start];
+    output_batch.z = &out->z[start];
+
+    batch_signals.batch_in = &input_batch;
+    batch_signals.batch_out = &output_batch;
+
+    return &batch_signals;
+}
+
+valid_output_t *get_valid_outputs(accel_data_t *output, accel_data_t *expected_output,
+                                  uint32_t valid_start, uint32_t N)
+{
+    static accel_data_t out_valid;
+
+    out_valid.num_samples = N - valid_start;
+    out_valid.x = &output->x[valid_start];
+    out_valid.y = &output->y[valid_start];
+    out_valid.z = &output->z[valid_start];
+
+    static accel_data_t expected_valid;
+
+    expected_valid.num_samples = N - valid_start;
+    expected_valid.x = &expected_output->x[valid_start];
+    expected_valid.y = &expected_output->y[valid_start];
+    expected_valid.z = &expected_output->z[valid_start];
+
+    valid_outputs.valid_output = &out_valid;
+    valid_outputs.valid_expected_output = &expected_valid;
+
+    return &valid_outputs;
+}
+
+void setUp(void) {
+    TEST_PREPROCESS_OK(preprocess_init(&preprocess));
+}
+
+void tearDown(void) {
+    if (scratch_buffer.x != NULL) {
+        free(scratch_buffer.x);
+        free(scratch_buffer.y);
+        free(scratch_buffer.z);
+    }
+
+    if (preprocess_output.x != NULL) {
+        free(preprocess_output.x);
+        free(preprocess_output.y);
+        free(preprocess_output.z);
+    }
+}
+
+void test_dc_signal_gravity_down(void) {
+    const uint32_t batch_size = 10;
+    const uint32_t N = gravity_down_input_len;
+
+    test_signal_t *test_signal = get_test_signals(gravity_down_input_x,
+                                                  gravity_down_input_y,
+                                                  gravity_down_input_z,
+                                                  gravity_down_output_x,
+                                                  gravity_down_output_y,
+                                                  gravity_down_output_z,
+                                                  N);
+
+    accel_data_t *scratch = get_scratch_buffer(batch_size);
+
+    for (uint32_t start = 0; start < N; start += batch_size) {
+        batch_signal_t *batch_signal = get_batch_signals(test_signal->input_signal,
+                                                         test_signal->output_signal,
+                                                         start,
+                                                         batch_size);
+
+        preprocess_status_t status = gravity_suppress_rotate(&preprocess,
+                                                             batch_signal->batch_in,
+                                                             scratch,
+                                                             batch_signal->batch_out);
+
+        if (start < FILTER_MEAN_GROUP_DELAY + 1) {
+            if (!(status == PREPROCESS_STATUS_ERROR_BUFFERING || status == PREPROCESS_STATUS_OK)) {
+                char message[128];
+
+                sprintf(message, "Unexpected preprocess_status_t value: %d", status);
+                TEST_FAIL_MESSAGE(message);
+            }
+        } else {
+            TEST_PREPROCESS_OK(status);
+        }
+    }
+
+    uint32_t valid_start = GRAVITY_DOWN_OUTPUT_VALID_START_IDX;
+
+    valid_output_t *valid_outputs = get_valid_outputs(test_signal->output_signal,
+                                                      test_signal->expected_output_signal,
+                                                      valid_start,
+                                                      N);
+
+#if DEBUG_DUMP_GRAVITY_OUTPUT
 /*
- *test_signal_t *get_test_signals(float32_t *x_in, float32_t *y_in, float32_t *z_in,
- *                                float32_t *x_out, float32_t *y_out, float32_t *z_out,
- *                                uint32_t num_samples)
- *{
- *    const uint32_t N = num_samples;
+ *    FILE *f = fopen("gravity_output.py", "w");
+ *    if (f) {
+ *        fprintf(f, "gravity_x = [\n");
+ *        for (uint32_t i = 0; i < out_valid.num_samples; ++i) {
+ *            fprintf(f, "    %.7g%s", out_valid.x[i], (i < out_valid.num_samples - 1) ? "," : "");
+ *            if ((i + 1) % 8 == 0 || i == out_valid.num_samples - 1) fprintf(f, "\n");
+ *        }
  *
- *    float32_t x[N];
- *    float32_t y[N];
- *    float32_t z[N];
+ *        fprintf(f, "]\n\ngravity_y = [\n");
+ *        for (uint32_t i = 0; i < out_valid.num_samples; ++i) {
+ *            fprintf(f, "    %.7g%s", out_valid.y[i], (i < out_valid.num_samples - 1) ? "," : "");
+ *            if ((i + 1) % 8 == 0 || i == out_valid.num_samples - 1) fprintf(f, "\n");
+ *        }
+ *
+ *        fprintf(f, "]\n\ngravity_z = [\n");
+ *        for (uint32_t i = 0; i < out_valid.num_samples; ++i) {
+ *            fprintf(f, "    %.7g%s", out_valid.z[i], (i < out_valid.num_samples - 1) ? "," : "");
+ *            if ((i + 1) % 8 == 0 || i == out_valid.num_samples - 1) fprintf(f, "\n");
+ *        }
+ *
+ *        fprintf(f, "]\n");
+ *        fclose(f);
+ *    } else {
+ *        printf("Failed to open gravity_output.py for writing\n");
+ *    }
+ */
+#endif
+
+    check_signal_close(&valid_outputs->valid_output,
+                       &valid_outputs->valid_expected_output,
+                       1e-2f);
+}
+
+/*
+ *void test_dc_signal_gravity_up_rotates_down(void) {
+ *    const uint32_t N = 100;
+ *
+ *    float32_t x[N] = {0};
+ *    float32_t y[N] = {0};
+ *    float32_t z[N] = {0};
  *
  *    for (uint32_t i = 0; i < N; i++) {
- *        x[i] = gravity_down_input_x[i];
- *        y[i] = gravity_down_input_y[i];
- *        z[i] = gravity_down_input_z[i];
+ *        z[i] = 1000; // 1g up
  *    }
  *
  *    accel_data_t signal = {
@@ -81,23 +277,6 @@ accel_data_t *get_scratch_buffer(uint32_t num_samples) {
  *        .x = x,
  *        .y = y,
  *        .z = z
- *    };
- *
- *    float x_expected[N];
- *    float y_expected[N];
- *    float z_expected[N];
- *
- *    for (uint32_t i = 0; i < N; ++i) {
- *        x_expected[i] = gravity_down_output_x[i];
- *        y_expected[i] = gravity_down_output_y[i];
- *        z_expected[i] = gravity_down_output_z[i];
- *    }
- *
- *    accel_data_t expected_output = {
- *        .num_samples = N,
- *        .x = x_expected,
- *        .y = y_expected,
- *        .z = z_expected
  *    };
  *
  *    float32_t x_out[N];
@@ -117,218 +296,29 @@ accel_data_t *get_scratch_buffer(uint32_t num_samples) {
  *        .z = z_out
  *    };
  *
- *    test_signals.expected_signal_in = &signal;
- *    test_signals.expected_signal_out = &expected_output;
- *    test_signals.output_signal = &out;
+ *    float32_t x_out_expected[N];
+ *    float32_t y_out_expected[N];
+ *    float32_t z_out_expected[N];
  *
- *    return &test_signals;
+ *    for (uint32_t i = 0; i < N; ++i) {
+ *        x_out_expected[i] = 0;
+ *        y_out_expected[i] = 0;
+ *        z_out_expected[i] = -1000; // 1g down
+ *    }
+ *
+ *    accel_data_t expected = {
+ *        .num_samples = N,
+ *        .x = x_out_expected,
+ *        .y = y_out_expected,
+ *        .z = z_out_expected
+ *    };
+ *
+ *    accel_data_t *scratch = get_scratch_buffer(N);
+ *    TEST_PREPROCESS_OK(gravity_suppress_rotate(&preprocess,
+ *                                               &signal,
+ *                                               scratch,
+ *                                               &out));
+ *
+ *    check_signal_close(&out, &expected, 1e-4f);
  *}
- *
  */
-void setUp(void) {
-    TEST_PREPROCESS_OK(preprocess_init(&preprocess));
-}
-
-void tearDown(void) {
-    if (scratch_buffer.x != NULL) {
-        free(scratch_buffer.x);
-        free(scratch_buffer.y);
-        free(scratch_buffer.z);
-    }
-}
-
-void test_dc_signal_gravity_down(void) {
-    const uint32_t batch_size = 10;
-    const uint32_t N = gravity_down_input_len;
-
-    float32_t x[N];
-    float32_t y[N];
-    float32_t z[N];
-
-    for (uint32_t i = 0; i < N; i++) {
-        x[i] = gravity_down_input_x[i];
-        y[i] = gravity_down_input_y[i];
-        z[i] = gravity_down_input_z[i];
-    }
-
-    accel_data_t signal = {
-        .num_samples = N,
-        .x = x,
-        .y = y,
-        .z = z
-    };
-
-    float x_expected[N];
-    float y_expected[N];
-    float z_expected[N];
-
-    for (uint32_t i = 0; i < N; ++i) {
-        x_expected[i] = gravity_down_output_x[i];
-        y_expected[i] = gravity_down_output_y[i];
-        z_expected[i] = gravity_down_output_z[i];
-    }
-
-    accel_data_t expected_output = {
-        .num_samples = N,
-        .x = x_expected,
-        .y = y_expected,
-        .z = z_expected
-    };
-
-    float32_t x_out[N];
-    float32_t y_out[N];
-    float32_t z_out[N];
-
-    for (uint32_t i = 0; i < N; ++i) {
-        x_out[i] = FLT_MAX;
-        y_out[i] = FLT_MAX;
-        z_out[i] = FLT_MAX;
-    }
-
-    accel_data_t out = {
-        .num_samples = N,
-        .x = x_out,
-        .y = y_out,
-        .z = z_out
-    };
-
-    accel_data_t *scratch = get_scratch_buffer(batch_size);
-
-    for (uint32_t start = 0; start < N; start += batch_size) {
-        accel_data_t input_batch = {
-            .num_samples = batch_size,
-            .x = &x[start],
-            .y = &y[start],
-            .z = &z[start]
-        };
-
-        accel_data_t output_batch = {
-            .num_samples = batch_size,
-            .x = &x_out[start],
-            .y = &y_out[start],
-            .z = &z_out[start]
-        };
-
-        preprocess_status_t status = gravity_suppress_rotate(&preprocess,
-                                                             &input_batch,
-                                                             scratch,
-                                                             &output_batch);
-
-        if (start < FILTER_MEAN_GROUP_DELAY + 1) {
-            if (!(status == PREPROCESS_STATUS_ERROR_BUFFERING || status == PREPROCESS_STATUS_OK)) {
-                char message[128];
-
-                sprintf(message, "Unexpected preprocess_status_t value: %d", status);
-                TEST_FAIL_MESSAGE(message);
-            }
-        } else {
-            TEST_PREPROCESS_OK(status);
-        }
-    }
-
-    uint32_t valid_start = GRAVITY_DOWN_OUTPUT_VALID_START_IDX;
-    accel_data_t out_valid = {
-        .num_samples = N - valid_start,
-        .x = &x_out[valid_start],
-        .y = &y_out[valid_start],
-        .z = &z_out[valid_start]
-    };
-
-    accel_data_t expected_valid = {
-        .num_samples = N - valid_start,
-        .x = &x_expected[valid_start],
-        .y = &y_expected[valid_start],
-        .z = &z_expected[valid_start]
-    };
-
-#if DEBUG_DUMP_GRAVITY_OUTPUT
-    FILE *f = fopen("gravity_output.py", "w");
-    if (f) {
-        fprintf(f, "gravity_x = [\n");
-        for (uint32_t i = 0; i < out_valid.num_samples; ++i) {
-            fprintf(f, "    %.7g%s", out_valid.x[i], (i < out_valid.num_samples - 1) ? "," : "");
-            if ((i + 1) % 8 == 0 || i == out_valid.num_samples - 1) fprintf(f, "\n");
-        }
-
-        fprintf(f, "]\n\ngravity_y = [\n");
-        for (uint32_t i = 0; i < out_valid.num_samples; ++i) {
-            fprintf(f, "    %.7g%s", out_valid.y[i], (i < out_valid.num_samples - 1) ? "," : "");
-            if ((i + 1) % 8 == 0 || i == out_valid.num_samples - 1) fprintf(f, "\n");
-        }
-
-        fprintf(f, "]\n\ngravity_z = [\n");
-        for (uint32_t i = 0; i < out_valid.num_samples; ++i) {
-            fprintf(f, "    %.7g%s", out_valid.z[i], (i < out_valid.num_samples - 1) ? "," : "");
-            if ((i + 1) % 8 == 0 || i == out_valid.num_samples - 1) fprintf(f, "\n");
-        }
-
-        fprintf(f, "]\n");
-        fclose(f);
-    } else {
-        printf("Failed to open gravity_output.py for writing\n");
-    }
-#endif
-
-    check_signal_close(&out_valid, &expected_valid, 1e-2f);
-}
-
-void test_dc_signal_gravity_up_rotates_down(void) {
-    const uint32_t N = 100;
-
-    float32_t x[N] = {0};
-    float32_t y[N] = {0};
-    float32_t z[N] = {0};
-
-    for (uint32_t i = 0; i < N; i++) {
-        z[i] = 1000; // 1g up
-    }
-
-    accel_data_t signal = {
-        .num_samples = N,
-        .x = x,
-        .y = y,
-        .z = z
-    };
-
-    float32_t x_out[N];
-    float32_t y_out[N];
-    float32_t z_out[N];
-
-    for (uint32_t i = 0; i < N; ++i) {
-        x_out[i] = FLT_MAX;
-        y_out[i] = FLT_MAX;
-        z_out[i] = FLT_MAX;
-    }
-
-    accel_data_t out = {
-        .num_samples = N,
-        .x = x_out,
-        .y = y_out,
-        .z = z_out
-    };
-
-    float32_t x_out_expected[N];
-    float32_t y_out_expected[N];
-    float32_t z_out_expected[N];
-
-    for (uint32_t i = 0; i < N; ++i) {
-        x_out_expected[i] = 0;
-        y_out_expected[i] = 0;
-        z_out_expected[i] = -1000; // 1g down
-    }
-
-    accel_data_t expected = {
-        .num_samples = N,
-        .x = x_out_expected,
-        .y = y_out_expected,
-        .z = z_out_expected
-    };
-
-    accel_data_t *scratch = get_scratch_buffer(N);
-    TEST_PREPROCESS_OK(gravity_suppress_rotate(&preprocess,
-                                               &signal,
-                                               scratch,
-                                               &out));
-
-    check_signal_close(&out, &expected, 1e-4f);
-}
