49101773adb30541aee4d48979b73a0e9cdfa9f4
Richard Matthews <richard_matthews@apple.com>
2025-11-15 18:11:26 -0500

    Add test files used to verify data logs on large recorded data (~150,000 samples)

---
+++ Recorded_Data/WALKING_OCT_21_RICHARD_input.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <math.h>
+#include "arm_math.h"
+
+#define WALKING_OCT_21_RICHARD_INPUT_DURATION_SEC 1597
+#define WALKING_OCT_21_RICHARD_INPUT_SAMPLE_RATE_HZ 100
+#define WALKING_OCT_21_RICHARD_INPUT_LEN 159750
+
+extern const uint32_t  WALKING_OCT_21_RICHARD_input_len;
+extern const float32_t WALKING_OCT_21_RICHARD_input_x[];
+extern const float32_t WALKING_OCT_21_RICHARD_input_y[];
+extern const float32_t WALKING_OCT_21_RICHARD_input_z[];
